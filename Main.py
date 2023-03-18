0ba54c1aa5e8dfb7c8d41573618e631048f7c6f1
Wilyhyperion <ns9316@pleasantonusd.net>
2023-03-18 11:46:57 -0700

    ids

--- Main.py
+++ Main.py
@@ -1,13 +1,19 @@
 from flask import app, current_app, Flask, request
 import os
 import requests
-requests.get("https://chat.openai.com/chat")
-import cohere
+import json 
 
-app = flask.Flask("Chess")
-@route("/")
+app = Flask("Chess")
+@app.route("/")
 def index():
     return open("Pages/index.html").read()
-app.route("/games/")
-def games():
-    return "Games";
+
+@app.route("/games/<id>")
+def games(id):
+    return "Games"
+@app.route("/newgame", methods=['POST'] )
+def newgame():
+    print("newgame")
+    return json.dumps({"id": getNewGameId()} )
+def getNewGameId():
+    return "1234"#Todo
