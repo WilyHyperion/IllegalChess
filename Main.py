9fb806d8b65af5fd1086c7916c246aca429b2061
Gabibag <97263038+Gabibag@users.noreply.github.com>
2023-03-18 11:43:45 -0700

    some changes

--- Main.py
+++ Main.py
@@ -1,11 +1,12 @@
 from flask import app, current_app, Flask, request
 import os
+import flask
 import requests
 requests.get("https://chat.openai.com/chat")
 import cohere
 
 app = flask.Flask("Chess")
-@route("/")
+@app.route("/")
 def index():
     return open("Pages/index.html").read()
 app.route("/games/")
