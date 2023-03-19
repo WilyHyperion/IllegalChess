ec9a8a40a5dd3bcf8876621e64d60065a2f73a1e
WilyHyperion <ns9316@pleasantonusd.net>
2023-03-19 01:42:24 -0700

    remove dupes

--- GPTScrapper.py
+++ GPTScrapper.py
@@ -5,7 +5,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver  
 from selenium.webdriver.chrome.service import Service as ChromeService 
