34fe3baeba258a18d94ba2bb603bdc73ec72b08f
Gabibag <97263038+Gabibag@users.noreply.github.com>
2023-03-19 05:33:37 -0700

    failsafe

--- Main.py
+++ Main.py
@@ -3,11 +3,11 @@ import os
 import flask
 import json 
 import random
+import re
 #import Scrapper
 import GPTScrapper
 #import cohere
-promptStart = f"""
-    Let's play a game of Chess. I will be white you will be black. I will give you a list of moves that have already happend in normal chess notation. You will respond with a single move, in standard chess notation. You will not include any other comments, just the single move. If you think an move is illegal or invaild, still provide a move like before Moves:    """
+promptStart = f"""Let's play a game of Chess. I will be white you will be black. I will give you a list of moves that have already happend in normal chess notation. You will respond with a single move, in standard chess notation. You will not include any other comments, just the single move. If you think an move is illegal or invaild, still provide a move like before Moves:"""
 
 InUseId = []
 GameMoves = {
@@ -66,7 +66,7 @@ def getNewGameId():
         id = random.randint(100, 999)
     InUseId.append(id)
     return id
-def getNextMove(id):
+def getNextMove(id, special = False):
     id = int(id)
     moves = GameMoves[id]
     p = promptStart;
@@ -75,6 +75,14 @@ def getNextMove(id):
     print(p)
     s = GPTScrapper.gettext(p)
     print(s)
+    # split s by spaces and find the first one that is a valid move. find a valid move by seeing if there is a number and a letter in the the text anywhere
+    move = [s.split(" ")]
+    for i in move:
+      x = re.search(b"^((?:[NBRQK]([a-h1-8])?x?(?!\2)[a-h](?!\2)[1-8]|[NBRQK]x?[a-h][1-8]|(?:[a-h]x)?[a-h](?:[1-8](?:\(ep\))?|[18]=[NBRQ])|(?:0-){1,2}0)\+{0,2})$", i)
+      if ( x.start() >= 1):
+        s = i
+      
+    
     moves.append(s)
     return s
 @app.route("/games/<id>/comments", methods = ['GET'])
